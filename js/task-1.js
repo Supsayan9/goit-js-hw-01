57144c60233a9670488a3b096114ed654817ef74
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2024-06-06 22:26:44 +0200

    +

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid){
-    const message = `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+    const message = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
    
    return message;
     
